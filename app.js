ef0899f22264ffe4edc7cf8b296278faa6ef00f0
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-20 11:34:37 +0100

    added css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 let items = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 	let today = new Date();
