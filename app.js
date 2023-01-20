eda5888b6518493aa87be1e5be83450b592360c5
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-20 01:23:31 +0100

    added views engin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/index.html");
