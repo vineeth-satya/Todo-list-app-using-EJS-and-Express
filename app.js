d7a245f5ad7c9ed328a433c32364fbed5815dbd3
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-20 01:11:37 +0100

    removed imports and used require

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-//jshint esversion:6
 
-import express from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+	// res.send("Hello");
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function () {
+	console.log("Server started on port 3000.");
 });
