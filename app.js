2e27a33ae34dc7ef99a236d07ed8c9882099ca54
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-01-20 10:41:41 +0100

    can now add new list items

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = [];
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/index.html");
-	// res.send("Hello");
+	let today = new Date();
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long"
+	};
+	let day = today.toLocaleDateString("en-US", options)
+
+	res.render("list", {
+		kindofDay: day,
+		newListItems: items
+	});
+});
+
+app.post("/", (req, res) => {
+	let item = req.body.newItem;
+	items.push(item);
+	res.redirect("/");
 });
 
 app.listen(3000, function () {
